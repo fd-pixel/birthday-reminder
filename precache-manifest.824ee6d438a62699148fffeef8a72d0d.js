ce6dec5b302af1f549068dce14fc1c4276d6b9de
FikretDuzceker <fikretduzceker5@gmail.com>
2021-06-22 14:17:37 +0300

    Updates

---
+++ precache-manifest.824ee6d438a62699148fffeef8a72d0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062f2adc5e45bc79c6765a80da5f8cdc",
+    "url": "/birthday-reminder/index.html"
+  },
+  {
+    "revision": "03d189e8fb3d10e847e5",
+    "url": "/birthday-reminder/static/css/main.995860c3.chunk.css"
+  },
+  {
+    "revision": "a9126082705d5b5d431b",
+    "url": "/birthday-reminder/static/js/2.daefb4dd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/birthday-reminder/static/js/2.daefb4dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03d189e8fb3d10e847e5",
+    "url": "/birthday-reminder/static/js/main.c4de37ce.chunk.js"
+  },
+  {
+    "revision": "5567b67584bb31e15f4d",
+    "url": "/birthday-reminder/static/js/runtime-main.35ffc63d.js"
+  }
+]);
